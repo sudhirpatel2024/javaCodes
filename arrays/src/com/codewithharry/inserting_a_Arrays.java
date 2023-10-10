dd57af5de1e4bf5c3d7c0bc518910ad5fa233049
Sudhir Kumar Patel <sudhir.patel2024@gmail.com>
2023-10-11 00:56:18 +0530

    Update inserting_a_Arrays.java

--- arrays/src/com/codewithharry/inserting_a_Arrays.java
+++ arrays/src/com/codewithharry/inserting_a_Arrays.java
@@ -3,7 +3,7 @@ package com.codewithharry;
 public class inserting_a_Arrays {
     public static void main(String[] args) {
         int a[]=new  int[10];
-        a[0]=5; a[1]=9; a[2]=6; a[3]=10; a[4]=12; a[5]=7;
+        a[0]=5; a[1]=9; a[2]=6; a[3]=10; a[4]=12; a[5]=7; // initializing in array using indexes
 
         int n=6;
         for (int i=0;i<n;i++)
